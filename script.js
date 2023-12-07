3a36f7e777ea4d15013aaf58cbd678543b58c257
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 22:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.body.appendChild(getSumBtn);
 const table = document.querySelector("table");
 
 function getSum() {
-  const price = document.querySelectorAll('[data-ns-test="price"]');
+  const price = document.querySelectorAll('.price');
   const tr = document.createElement("tr");
   const td1 = document.createElement("td");
-  td1.setAttribute("data-ns-test", "grandTotal");
+  // td1.setAttribute("data-ns-test", "grandTotal");
   // const td2 =  document.createElement("td");
   let sum = 0;
   for (let i = 0; i < price.length; i++) {
