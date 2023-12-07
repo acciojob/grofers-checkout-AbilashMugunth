90171396872720d7ff1cf77119776fcae47c62dc
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 21:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+let table = document.querySelector("table");
+let prices = document.querySelectorAll(".price");
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	let total = Array.from(prices)
+	  .map((price) => Number(price.innerText))
+	  .reduce((acc, curr) => {
+	    return acc + curr;
+	  }, 0);
+	
+	let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	td.innerText = total;
+	
+	tr.appendChild(td);
+	table.appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
+
 
