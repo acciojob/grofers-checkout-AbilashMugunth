ec2e54589814d3a1821687c51409d64b272f4912
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-08 14:07:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,27 +2,31 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const getSum = () => {
+  // Select all elements with the class "price" to get the prices
+  const priceElements = document.querySelectorAll(".price");
 
-function getSum() {
-	let table = document.querySelector("table");
-	let prices = document.querySelectorAll(".price");
-	
-	let total = Array.from(prices)
-	  .map((price) => Number(price.innerText))
-	  .reduce((acc, curr) => {
-	    return acc + curr;
-	  }, 0);
-	
+  // Calculate the total price
+  let totalPrice = 0;
+  priceElements.forEach((priceElement) => {
+    const price = parseFloat(priceElement.textContent);
+    if (!isNaN(price)) {
+      totalPrice += price;
+    }
+  });
+
+  // Create a new row for the total price
+  const table = document.querySelector("table");
   const newRow = table.insertRow(table.rows.length);
   const cell1 = newRow.insertCell(0);
   const cell2 = newRow.insertCell(1);
 
   // Set the cell values
   cell1.textContent = "Total Price";
-  cell2.textContent = total;
+  cell2.textContent = totalPrice;
 
   // Style the new row
   newRow.style.fontWeight = "bold";
-}
+};
 
 getSumBtn.addEventListener("click", getSum);
