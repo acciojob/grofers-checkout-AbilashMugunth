ddf516b04c41f5413ea8537c320893a3dddbe169
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-08 14:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,31 +2,26 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-  // Select all elements with the class "price" to get the prices
-  const priceElements = document.querySelectorAll(".price");
 
-  // Calculate the total price
-  let totalPrice = 0;
-  priceElements.forEach((priceElement) => {
-    const price = parseFloat(priceElement.textContent);
-    if (!isNaN(price)) {
-      totalPrice += price;
-    }
-  });
-
-  // Create a new row for the total price
-  const table = document.querySelector("table");
-  const newRow = table.insertRow(table.rows.length);
-  const cell1 = newRow.insertCell(0);
-  const cell2 = newRow.insertCell(1);
-
-  // Set the cell values
-  cell1.textContent = "Total Price";
-  cell2.textContent = totalPrice;
-
-  // Style the new row
-  newRow.style.fontWeight = "bold";
-};
+function getSum() {
+	let table = document.querySelector("table");
+	let prices = document.querySelectorAll(".price");
+	
+	let total = Array.from(prices)
+	  .map((price) => Number(price.innerText))
+	  .reduce((acc, curr) => {
+	    return acc + curr;
+	  }, 0);
+	
+	let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	td.innerText = "Total Price";
+	let td2 = document.createElement("td");
+	td2.colSpan=2;
+	td2.innerText = total;
+	
+	tr.append(td, td2);
+	table.appendChild(tr);
+}
 
 getSumBtn.addEventListener("click", getSum);
